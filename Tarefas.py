beeaeb857ca0eadabd5ed99a183be80ee1e6606c
Eduarda <0178263@sesi-es.org.br>
2026-06-17 07:48:16 -0300

    feat: cria estrutura base do To-Do App

---
+++ Tarefas.py
@@ -0,0 +1,14 @@
+class Tarefas:
+    def _init_(self):
+        self.lista = [ ]
+
+def adicionar(self, nome):
+  self. lista. append ({
+     "nome": nome,
+"concluida": False
+  })
+
+def listar(self):
+    for i, t in enumerate(self.lista, 1):
+        s = "V" if t["concluida"] else "o"
+        print(f"{i}. [{s}] {t['nome']}")
