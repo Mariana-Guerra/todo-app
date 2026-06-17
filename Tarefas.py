1c3adb1b97ad5974bd30f9bc73e546065c123eb9
Eduarda <0178263@sesi-es.org.br>
2026-06-17 08:07:33 -0300

    feat: adiciona campo de prioridade nas tarefas

--- Tarefas.py
+++ Tarefas.py
@@ -12,3 +12,19 @@ def listar(self):
     for i, t in enumerate(self.lista, 1):
         s = "V" if t["concluida"] else "o"
         print(f"{i}. [{s}] {t['nome']}")
+
+def adicionar(self, nome, prioridade="normal"):
+     self.lista.append({
+            "nome": nome,
+            "concluida": False,
+            "prioridade": prioridade
+        })
+
+def listar(self):
+        if not self.lista:
+            print("Nenhuma tarefa cadastrada.")
+            return
+        for i, t in enumerate(self.lista, 1):
+            s = "✓" if t["concluida"] else "○"
+            p = t.get("prioridade", "normal")
+            print(f"{i}. [{s}] {t['nome']} ({p})")
